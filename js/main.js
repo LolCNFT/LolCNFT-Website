ee61a5f884dd1054d4dd286e9989030bde73e30d
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-05-14 15:49:57 -0300

    add contact me section

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ function myFunction() {
     $("#image-banner").mousemove(function(e){
               var pageX = e.pageX - ($(window).width() / 2);
               var pageY = e.pageY - ($(window).height() / 2);
-              var newvalueX = width * pageX * -1 - 20;
-              var newvalueY = height * pageY * -1 - 0;
+              var newvalueX = width * pageX * -1 - 10;
+              var newvalueY = height * pageY * -1 - 10;
               $('#image-banner').css("background-position", newvalueX+"px     "+newvalueY+"px");
     });
     $("#image-banner-2").mousemove(function(e){
