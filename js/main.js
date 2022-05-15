b88291bc365ac3cedd4d1893168e135024d73e66
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-05-15 10:24:08 -0300

    witb

--- js/main.js
+++ js/main.js
@@ -57,15 +57,61 @@ $("#image-banner-4").mousemove(function(e){
   $('#image-banner-4').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
   });
-    // $(document).ready(function() {
-    //   var movementStrength = 55;
-    //   var height = movementStrength / $(window).height();
-    //   var width = movementStrength / $(window).width();
-    //   $("#image-banner-2").mousemove(function(e){
-    //             var pageX = e.pageX - ($(window).width() / 2);
-    //             var pageY = e.pageY - ($(window).height() / 2);
-    //             var newvalueX = width * pageX * -1 - 25;
-    //             var newvalueY = height * pageY * -1 - 50;
-    //             $('#image-banner-2').css("background-position", newvalueX+"px     "+newvalueY+"px");
-    //   });
-    //   });
+
+  //copy txt
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(
+      function() {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function(err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  }
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+  var copyBtn = document.querySelector(".copy-txt");
+
+copyBtn.addEventListener("click", function(event) {
+  copyTextToClipboard('5ded942a9ae524354dbba9101cc8705b5d1926238eb55b1e3415fcbd');
+});
+
+/*-----------------------------------------------------------
+2. Modal
+-------------------------------------------------------------*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
