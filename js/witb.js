c5ce9713f836a1213898606d300303c449299f38
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-05-28 10:06:49 -0300

    update box

--- js/witb.js
+++ js/witb.js
@@ -1,5 +1,8 @@
 /*box*/
+var land = [];
+var number = 0
 var input = document.getElementById('file-input');
+var i = 0;
 input.onchange = e => { 
   // getting a hold of the file reference
   var file = e.target.files[0]; 
@@ -7,24 +10,47 @@ input.onchange = e => {
   var reader = new FileReader();
   reader.readAsDataURL(file); // this is reading as data url
   // here we tell the reader what to do when it's done reading...
-  var back =  document.getElementById('coverback');
-      var right =  document.getElementById('coverright');
-      var left =  document.getElementById('coverleft'); 
-      var front =  document.getElementById('coverfront');
   reader.onload = readerEvent => {
-      var content = readerEvent.target.result; // this is the content!
-      document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
-      document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
-      document.querySelector('#backk').style.backgroundImage = 'url('+ content +')';
-      document.querySelector('#tap').style.backgroundImage = 'url('+ content +')';
-      document.querySelector('#front').style.backgroundImage = 'url('+ content +')';
-      back.style.backgroundImage = 'url('+ content +')';
-      right.style.backgroundImage = 'url('+ content +')';
-      left.style.backgroundImage = 'url('+ content +')';
-      front.style.backgroundImage = 'url('+ content +')';
+      if ( i < 5){
+        var content = readerEvent.target.result; // this is the content!
+        land[i] = content;
+        document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#backk').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#tap').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#front').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverback').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverright').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverleft').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverfront').style.backgroundImage = 'url('+ content +')';
+        i++
+      }
     }
 }
-var input2 = document.getElementById('file-input-2');
+
+function change(){
+  length = land.length
+  if (length == 0 || length == 1){
+    return
+  }
+var landscape = Math.floor(Math.random() * length);
+      while (number == landscape){
+        landscape = Math.floor(Math.random() * length);
+      }
+      number = landscape;
+       
+      document.querySelector('#left').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#rightt').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#backk').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#tap').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#front').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#coverback').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#coverright').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#coverleft').style.backgroundImage = 'url('+ land[landscape] +')';
+      document.querySelector('#coverfront').style.backgroundImage = 'url('+ land[landscape] +')';
+    }
+
+    var input2 = document.getElementById('file-input-2');
 input2.onchange = e => { 
   // getting a hold of the file reference
   var file = e.target.files[0]; 
@@ -64,72 +90,77 @@ box.addEventListener("drop", function( event) {
   event.preventDefault();
   event.stopPropagation();
   const file = event.dataTransfer.files[0];
-  let newURL = URL.createObjectURL( file);
-  if( oldURL) {
-      URL.revokeObjectURL(oldURL);
-  }
-  oldURL = newURL;
-  document.getElementById("front")
-  .style.backgroundImage = `url("${newURL}")`;
-  document.getElementById("rightt")
-  .style.backgroundImage = `url("${newURL}")`;
+  var reader = new FileReader();
+  reader.readAsDataURL(file); // this is reading as data url
+  // here we tell the reader what to do when it's done reading...
+  reader.onload = readerEvent => {
+    if ( i < 5){
+        var content = readerEvent.target.result; // this is the content!
+        land[i] = content;
+        document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#backk').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#tap').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#front').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverback').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverright').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverleft').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverfront').style.backgroundImage = 'url('+ content +')';
+        i++
+      }if (i == 5){
+        i = 0;
+        var content = readerEvent.target.result; // this is the content!
+        land[i] = content;
+        document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#backk').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#tap').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#front').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverback').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverright').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverleft').style.backgroundImage = 'url('+ content +')';
+        document.querySelector('#coverfront').style.backgroundImage = 'url('+ content +')';
+        i++
+      }
+    }
 
-document.getElementById("left")
-.style.backgroundImage = `url("${newURL}")`;
 
-document.getElementById("backk")
-.style.backgroundImage = `url("${newURL}")`;
-
-document.getElementById("coverleft")
-.style.backgroundImage = `url("${newURL}")`;
-
-document.getElementById("coverright")
-.style.backgroundImage = `url("${newURL}")`;
-
-document.getElementById("coverfront")
-.style.backgroundImage = `url("${newURL}")`;
-
-document.getElementById("coverback")
-.style.backgroundImage = `url("${newURL}")`;
-
-document.getElementById("tap")
-.style.backgroundImage = `url("${newURL}")`;
 });
+
+
+
 let flag = 0;
+
 function openLid(){
 var back =  document.getElementById('coverback');
 var right =  document.getElementById('coverright');
 var left =  document.getElementById('coverleft'); 
 var front =  document.getElementById('coverfront');
 let content = document.getElementById('content');
-var txt = document.getElementById('txt')
-var txt2 = document.getElementById('txt2')
-var txt3 = document.getElementById('txt3')
+// let hand = document.getElementById('hand')
+// let finger1 = document.getElementsByClassName('finger');
 if (flag == 0){
-flag = 1;
-front.classList.add('front-cover');
+  flag = 1;
+  front.classList.add('front-cover');
 
-back.classList.add('back-cover');
-left.classList.add('left-cover');
-right.classList.add('right-cover');
-content.classList.add('content-active');
-txt.classList.add('txt-active');
-txt2.classList.add('txt2-active');
-txt3.classList.add('txt3-active');
+  back.classList.add('back-cover');
+  left.classList.add('left-cover');
+  right.classList.add('right-cover');
+  content.classList.add('content-active');
+//     hand.classList.add('hand-active');
+
 }
 else {
-flag = 0;
-front.classList.remove('front-cover');
-back.classList.remove('back-cover');
-left.classList.remove('left-cover');
-right.classList.remove('right-cover');
-content.classList.remove('content-active');
-txt.classList.remove('txt-active');
-txt2.classList.remove('txt2-active');
-txt3.classList.remove('txt3-active');
-}
+  flag = 0;
+  front.classList.remove('front-cover');
+  back.classList.remove('back-cover');
+  left.classList.remove('left-cover');
+  right.classList.remove('right-cover');
+  content.classList.remove('content-active');
 
 }
+}
+input.click();
   //copy txt
   function copyTextToClipboard(text) {
     if (!navigator.clipboard) {
