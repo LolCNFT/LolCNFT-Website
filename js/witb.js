3a3b44cb2332cc51c81382a4cc1b9c4b6a4f1123
LolCNFT <89092716+independetrader@users.noreply.github.com>
2022-05-28 12:01:45 -0300

    update instructions

--- js/witb.js
+++ js/witb.js
@@ -4,15 +4,12 @@ var number = 0
 var input = document.getElementById('file-input');
 var i = 0;
 input.onchange = e => { 
-  // getting a hold of the file reference
   var file = e.target.files[0]; 
-  // setting up the reader
   var reader = new FileReader();
-  reader.readAsDataURL(file); // this is reading as data url
-  // here we tell the reader what to do when it's done reading...
+  reader.readAsDataURL(file);
   reader.onload = readerEvent => {
       if ( i < 5){
-        var content = readerEvent.target.result; // this is the content!
+        var content = readerEvent.target.result;
         land[i] = content;
         document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
         document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
@@ -27,7 +24,6 @@ input.onchange = e => {
       }
     }
 }
-
 function change(){
   length = land.length
   if (length == 0 || length == 1){
@@ -38,7 +34,6 @@ var landscape = Math.floor(Math.random() * length);
         landscape = Math.floor(Math.random() * length);
       }
       number = landscape;
-       
       document.querySelector('#left').style.backgroundImage = 'url('+ land[landscape] +')';
       document.querySelector('#rightt').style.backgroundImage = 'url('+ land[landscape] +')';
       document.querySelector('#backk').style.backgroundImage = 'url('+ land[landscape] +')';
@@ -49,17 +44,13 @@ var landscape = Math.floor(Math.random() * length);
       document.querySelector('#coverleft').style.backgroundImage = 'url('+ land[landscape] +')';
       document.querySelector('#coverfront').style.backgroundImage = 'url('+ land[landscape] +')';
     }
-
     var input2 = document.getElementById('file-input-2');
 input2.onchange = e => { 
-  // getting a hold of the file reference
   var file = e.target.files[0]; 
-  // setting up the reader
   var reader = new FileReader();
-  reader.readAsDataURL(file); // this is reading as data url
-  // here we tell the reader what to do when it's done reading...
+  reader.readAsDataURL(file);
   reader.onload = readerEvent => {
-      var content = readerEvent.target.result; // this is the content!
+      var content = readerEvent.target.result;
       document.querySelector('#bk').style.backgroundImage = 'url('+ content +')';
     }
 }
@@ -91,11 +82,10 @@ box.addEventListener("drop", function( event) {
   event.stopPropagation();
   const file = event.dataTransfer.files[0];
   var reader = new FileReader();
-  reader.readAsDataURL(file); // this is reading as data url
-  // here we tell the reader what to do when it's done reading...
+  reader.readAsDataURL(file);
   reader.onload = readerEvent => {
     if ( i < 5){
-        var content = readerEvent.target.result; // this is the content!
+        var content = readerEvent.target.result;
         land[i] = content;
         document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
         document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
@@ -109,7 +99,7 @@ box.addEventListener("drop", function( event) {
         i++
       }if (i == 5){
         i = 0;
-        var content = readerEvent.target.result; // this is the content!
+        var content = readerEvent.target.result;
         land[i] = content;
         document.querySelector('#left').style.backgroundImage = 'url('+ content +')';
         document.querySelector('#rightt').style.backgroundImage = 'url('+ content +')';
@@ -123,14 +113,8 @@ box.addEventListener("drop", function( event) {
         i++
       }
     }
-
-
 });
-
-
-
 let flag = 0;
-
 function openLid(){
 var back =  document.getElementById('coverback');
 var right =  document.getElementById('coverright');
@@ -140,8 +124,6 @@ let content = document.getElementById('content');
 let txt = document.getElementById('txt');
 let txt2 = document.getElementById('txt2');
 let txt3 = document.getElementById('txt3');
-// let hand = document.getElementById('hand')
-// let finger1 = document.getElementsByClassName('finger');
 if (flag == 0){
   flag = 1;
   front.classList.add('front-cover');
@@ -152,8 +134,6 @@ if (flag == 0){
   txt.classList.add('txt-active');
   txt2.classList.add('txt2-active');
   txt3.classList.add('txt3-active');
-//     hand.classList.add('hand-active');
-
 }
 else {
   flag = 0;
@@ -225,20 +205,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-// function alert(){
-//   const message = document.getElementById(alert);
-//   message.style.opacity = 1;
-//   message.style.display = 'inline';
-//   if (message.style.opacity != 0){
-//     message.style.opacity -= 1;
-//   } 
-//   else{
-//     message.style.display = 'none';
-//   }
-// }
-
-
-
 function myFunctionCopy() {
     /* Get the text field */
     var copyText = document.getElementById("myInput");
